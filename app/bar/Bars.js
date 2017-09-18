a9eed9c8074d2f988cdc7b05e89456fa27ec1fff
Igor Barsi <ibarsi92@gmail.com>
2017-09-18 19:31:45 -0400

    Added scroll to refresh list of bars.

--- app/bar/Bars.js
+++ app/bar/Bars.js
@@ -1,35 +1,53 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, RefreshControl } from 'react-native';
 import { List } from 'react-native-elements';
 
 import { getBarsByLocation } from './bars.service';
 import BarListItem from './BarListItem';
+
+const getLocations = async() => {
+    const bars = await getBarsByLocation();
+
+    return bars.sort((a, b) => a.distance - b.distance);
+};
 
 export default class Bars extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            bars: []
+            bars: [],
+            refreshing: false
         };
+
+        this.onRefresh = this._onRefresh.bind(this);
     }
 
     async componentWillMount() {
-        const bars = await getBarsByLocation()
-            .sort((a, b) => a.distance - b.distance);
-
         this.setState({
-            bars
+            bars: await getLocations()
+        });
+    }
+
+    async _onRefresh() {
+        this.setState({
+            bars: await getLocations()
         });
     }
 
     render() {
-        return <View>
+        return <ScrollView
+            refreshControl={
+                <RefreshControl
+                    refreshing={ this.state.refreshing }
+                    onRefresh={ this.onRefresh }
+                />
+            }>
             <List>
                 {
                     this.state.bars.map((bar, index) => <BarListItem bar={ bar } key={ index } />)
                 }
             </List>
-        </View>;
+        </ScrollView>;
     }
 }
